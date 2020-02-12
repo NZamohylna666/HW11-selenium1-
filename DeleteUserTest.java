dba68b7d888e0b9e06c55f1484f1c9a99fb45338
NZamohylna666 <zamohylnana@gmail.com>
2020-02-12 18:08:31 +0200

    add test

---
+++ DeleteUserTest.java
@@ -0,0 +1,56 @@
+package hw.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class DeleteUserTest {
+    private String url;
+    private WebDriver driver;
+    private String phone;
+    private String name;
+
+    @BeforeClass
+    public void beforeMethod() {
+        url = "http://37.59.228.229:3000/";
+        driver = new ChromeDriver();
+        phone = "123456";
+        name = "Kenny";
+    }
+
+    @Test
+    public void testMethod() {
+        driver.get(url);
+        driver.findElement(By.cssSelector("i.mdi-content-add")).click();
+        driver.findElement(By.cssSelector("div.main-content input#icon_prefix")).clear();
+        driver.findElement(By.cssSelector("div.main-content input#icon_prefix")).sendKeys(name);
+        driver.findElement(By.cssSelector("div.main-content input#icon_telephone")).clear();
+        driver.findElement(By.cssSelector("div.main-content input#icon_telephone")).sendKeys(phone);
+        driver.findElement(By.cssSelector("div.main-content a.save-btn")).click();
+        driver.getPageSource();
+
+        driver.findElement(By.xpath(("//p[text() = '123456']/preceding::i[1]"))).click();
+
+        driver.switchTo().alert().accept();
+    }
+
+
+        @AfterClass
+        public void afterMethod () {
+            driver.quit();
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
