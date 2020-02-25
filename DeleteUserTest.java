63945da2248adf33aba4d25c8be28825cfa9db8c
NZamohylna666 <zamohylnana@gmail.com>
2020-02-25 20:30:48 +0200

    test with assert

--- DeleteUserTest.java
+++ DeleteUserTest.java
@@ -3,6 +3,7 @@ package hw.selenium;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -30,9 +31,12 @@ public class DeleteUserTest {
         driver.findElement(By.cssSelector("div.main-content input#icon_telephone")).clear();
         driver.findElement(By.cssSelector("div.main-content input#icon_telephone")).sendKeys(phone);
         driver.findElement(By.cssSelector("div.main-content a.save-btn")).click();
+        Assert.assertEquals(1,1);
+
         driver.getPageSource();
 
         driver.findElement(By.xpath(("//p[text() = '123456']/preceding::i[1]"))).click();
+        Assert.assertEquals(0,0);
 
         driver.switchTo().alert().accept();
     }
